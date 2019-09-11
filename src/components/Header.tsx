78f8a611bda23a7a8cb16299cf66cf47a3fbfe76
Stanislas Bernard <bernardstanislas@gmail.com>
2019-09-11 20:06:09 +0200

    :lipstick: Add margins

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 
 const Header : React.FunctionComponent = () => (
     <div className="sticky top-0 bg-gray-900 z-10">
-        <Link to="/"><div className="text-center text-4xl lg:text-5xl text-white tracking-widest">INPF</div></Link>
+        <Link to="/"><div className="text-center text-4xl lg:text-5xl text-white tracking-widest no-underline">INPF</div></Link>
     </div>
 );
 
